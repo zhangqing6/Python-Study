d0a448ac1a4c8c131e128722470fc68fb0b54c74
zhangqing6 <141533401+zhangqing6@users.noreply.github.com>
2026-04-16 22:08:35 +0800

    getIntersectionNode.py

---
+++ getIntersectionNode.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        pA,pB=headA,headB
+        while pA!=pB:
+            pA=pA.next if pA else headB
+            pB=pB.next if pB else headA
+        return pB

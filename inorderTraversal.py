63379319267e268e73791a6c9fe93f0548bf1fdc
zhangqing6 <141533401+zhangqing6@users.noreply.github.com>
2026-04-21 19:58:13 +0800

    Create inorderTraversal.py

---
+++ inorderTraversal.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        res=[]
+        stack=[]
+        cur=root
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur=cur.left
+            cur=stack.pop()
+            res.append(cur.val)
+            cur=cur.right
+        return res

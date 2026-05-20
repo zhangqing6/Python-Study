f63a78fd0c109747042b6ea8b1c0c0fe1e4c4eba
zhangqing6 <141533401+zhangqing6@users.noreply.github.com>
2026-05-20 20:25:56 +0800

    Create  climbStairs.py

---
+++ climbStairs.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n==1:
+            return 1
+        if n==2:
+            return 2
+        first=1
+        second=2
+        for i in range(3,n+1):
+            third=first+second
+            first=second
+            second=third
+        return second

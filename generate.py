0d9313e318feb2bf0bd7d4ca043d30bb59115469
zhangqing6 <141533401+zhangqing6@users.noreply.github.com>
2026-05-21 21:22:44 +0800

    Create generate.py

---
+++ generate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        res=[]
+        for i in range(numRows):
+            row=[1]*(i+1)
+            for j in range(1,i):
+                row[j]=res[i-1][j-1]+res[i-1][j]
+            res.append(row)
+        return res

78b14f1a416e4bb7fe089f0778c707341b89a2cd
zhangqing6 <141533401+zhangqing6@users.noreply.github.com>
2026-05-26 21:12:29 +0800

    Create coinChange.py

---
+++ coinChange.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        dp=[float('inf')]*(amount+1)
+        dp[0]=0
+        for i in range(1,amount+1):
+            for coin in coins:
+                if coin<=i and dp[i-coin]!=float('inf'):
+                    dp[i]=min(dp[i],dp[i-coin]+1)
+        return dp[amount] if dp[amount]!=float('inf') else -1

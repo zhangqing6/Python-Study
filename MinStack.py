2a4ccffba219c51f13822345876020550f64cc20
zhangqing6 <141533401+zhangqing6@users.noreply.github.com>
2026-06-30 23:34:04 +0800

    Update MinStack.py

--- MinStack.py
+++ MinStack.py
@@ -35,9 +35,9 @@ class MinStack(object):
         return self.min_stack[-1]
 
 
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+ # Your MinStack object will be instantiated and called as such:
+ # obj = MinStack()
+ # obj.push(val)
+ # obj.pop()
+ # param_3 = obj.top()
+ # param_4 = obj.getMin()
